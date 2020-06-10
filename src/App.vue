a29f1193f2864e90349ed0ab896d99de63d78098
Proboss <64521451+xac-amka@users.noreply.github.com>
2020-06-10 10:36:03 +0800

    :octocat: first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <ProductListOne />
+    <ProductListTwo />
+  </div>
+</template>
+
+<script>
+import ProductListOne from './components/ProductListOne'
+import ProductListTwo from './components/ProductListTwo'
+export default {
+  name: 'app',
+  components : {
+    ProductListOne,
+    ProductListTwo
+  },
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  font-family: Ubuntu;
+  color: #555555;
+}
+</style>
